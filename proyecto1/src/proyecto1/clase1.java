b4e5b4e4320efcf76486ff1a8629dc56e3a193f0
jjsalas1601 <70911749+jjsalas1601@users.noreply.github.com>
2021-01-05 07:28:38 -0600

    Update clase1.java
    modificacion clase1

--- proyecto1/src/proyecto1/clase1.java
+++ proyecto1/src/proyecto1/clase1.java
@@ -3,8 +3,12 @@ package proyecto1;
 public class clase1 {
 
 	public void holaa() {
+		System.out.println("hola mundo1");
 		System.out.println("hola mundo2");
 		System.out.println("hola mundo3");
 		System.out.println("hola mundo4");
+		System.out.println("hola mundo5");
+		System.out.println("hola mundo6");
+		System.out.println("hola mundo7");
 	}
 }
